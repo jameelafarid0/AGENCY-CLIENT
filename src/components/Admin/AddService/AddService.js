b8b47be296b2395b58b5bac719f9894d4f0b565b
jameelafarid0 <jameelafarid9@gmail.comm>
2020-10-16 20:46:46 +0300

    first commit

--- src/components/Admin/AddService/AddService.js
+++ src/components/Admin/AddService/AddService.js
@@ -1,12 +1,14 @@
 import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { UserContext } from '../../../App';
 import image from '../../../images/logos/logo.png';
 import SideBar from '../../ClientDashboard/SideBar/SideBar';
 
 const AddService = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-    const [file, setFile] = useState(null);
+    const { register, errors } = useForm();
     const [info, setInfo] = useState({});
+    const [file, setFile] = useState(null);
 
     const handleBlur = e => {
         const newInfo = { ...info };
@@ -14,17 +16,16 @@ const AddService = () => {
         setInfo(newInfo);
     }
 
-    const handleFileChange = (e) => {
+    const handleFileChange = e => {
         const newFile = e.target.files[0];
         setFile(newFile);
-
     }
 
     const handleSubmit = () => {
         const formData = new FormData()
-        formData.append('file', file);
-        formData.append('title', info.title);
-        formData.append('description', info.description); 
+        formData.append('file', file)
+        formData.append('name', info.name)
+        formData.append('description', info.description)
 
         fetch('https://peaceful-forest-05385.herokuapp.com/addService', {
             method: 'POST',
@@ -41,46 +42,60 @@ const AddService = () => {
 
     return (
         <div>
-            <div>
-                <nav className="navbar  navbar-expand-lg navbar-light navbar-fixed-top pt-3">
-                    <div class="d-flex">
-                        <img style={{ width: '130px' }} src={image} alt="" />
-                        <div className="ml-5 pl-5 " style={{ display: "flex" }}>
-                            <h3>Add Services</h3>
-                            <h5 style={{ color: "gray", marginLeft: "700px" }} >{loggedInUser.name}</h5>
-
-                        </div>
+            <nav className="navbar navbar-expand-lg navbar-light navbar-fixed-top pt-3">
+                <div class="d-flex">
+                    <img style={{ width: '130px' }} src={image} alt="" />
+                    <div className="ml-5 pl-5 d-flex" >
+                        <h3>Add Services</h3>
+                        <h4 style={{
+                            marginLeft: '850px'
+                        }}>{loggedInUser.username}</h4>
                     </div>
-                </nav>
-
-            </div>
+                </div>
+            </nav>
             <div className="row">
                 <div className="col-md-2 col-sm-6 col-12">
                     <SideBar></SideBar>
                 </div>
-                <div className="container-fluid col-md-10" style={{ backgroundColor: '#f4f7fc', height: '574px' }}>
-                    <form onSubmit={handleSubmit}>
-                        <div class="form-group">
-                            <label >Service title</label>
-                            <input onBlur={handleBlur} type="text" class="form-control" name="title" placeholder="Enter title" />
-
+                <div className="col-md-10" style={{
+                    backgroundColor:
+                        '#E5E5E5', height: '100vh'
+                }}>
+                    <form className="m-3" onSubmit={handleSubmit}>
+                        <div className="bg-white p-5 container-fluid row submit-area">
+                            <div className="col-md-6">
+                                <div class="form-group">
+                                    <label
+                                        htmlFor=""><strong><h5>Service Title</h5></strong></label>
+                                    <input onBlur={handleBlur}
+                                        type="text" ref={register({ required: true })} name="name"
+                                        className="form-control" placeholder="Enter title" />
+                                    {errors.title && <span
+                                        className="text-danger">This field is required</span>}
+                                </div>
+                                <div className="form-group">
+                                    <label
+                                        htmlFor=""><strong><h5>Description</h5></strong></label>
+                                    <textarea onBlur={handleBlur}
+                                        type="text" ref={register({ required: true })} name="description"
+                                        className="form-control" cols="30" rows="6" placeholder="Enter Description"></textarea>
+                                    {errors.description && <span
+                                        className="text-danger">This field is required</span>}
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <label
+                                    htmlFor=""><strong><h5>Icon</h5></strong></label>
+                                <input onChange={handleFileChange}
+                                    type="file" className="btn btn-success form-control-lg" />
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label >Description</label>
-                            <input onBlur={handleBlur} type="text" class="form-control" name="description" placeholder="Enter designation" />
+                        <div className="row d-flex justify-content-end mr-3 mt-3">
+                            <button type="submit" class="btn btn-success" >Submit</button>
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Upload image</label>
-                            <input onChange={handleFileChange} type="file" class="form-control" id="exampleInputPassword1" placeholder="Enter designation" />
-                        </div>
-
-                        <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
-
             </div>
-
-
         </div>
     );
 };
